98399d3117f2d0a5367ed5d92deb8a7731acffe8
Gossink <a1822415@adelaide.edu.au>
2022-05-12 15:36:15 +0930

    add Player Classes

---
+++ Player_files/Human.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Human.h"
+
+Human::Human(){}                       
+        
+void Human::playerMove(){}
+
+string Human::getMoves(){} 
+
+void Human::hit(){}
+
+int Human::getCount(){}
+                                    
+Human::~Human(){}
