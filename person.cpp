19db8d0f5a039df29f2d2239f459dd77e45ae625
a1822480 <a1822480@student.adelaide.edu.au>
2022-05-12 15:47:55 +0930

    start

---
+++ person.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "person.h"
+using namespace std;
+
+
+    // card *card array;
+    // int CardTotal;
+    
+person::Person(){
+    card = [];
+    CardTotal=0;
+}
+
+int person::getMove(){
+    // retrieve the move of 
+}
+
+int person::getCount(){
+    return CardTotal;
+}
+
+void person::hit(){
+    // what will happen when someone hits?
+    //
+}
+
+bool person::winGame(){
+
+}
+
+person::~Person(){
+
+}
