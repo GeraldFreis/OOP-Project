c8086a695792dbc4be927525a1d9656888e4521f
Gerald Freislich <freislichg@gmail.com>
2022-05-12 19:48:34 +0930

    safe adding blackjack

---
+++ Game_files/blackjack.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ncurses.h>
+#include <form.h>
+using std::string;
+
+#ifndef BLACKJACK_H
+#define BLACKJACK_H
+
+/*
+Blackjack
+
+*/
+class Blackjack{
+
+};
+
+#endif
