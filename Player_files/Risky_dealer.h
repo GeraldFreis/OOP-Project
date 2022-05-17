df5485bfc9689bb37715ab986112361380ed1aa5
Gerald Freislich <freislichg@gmail.com>
2022-05-17 09:34:57 +0930

    safe

---
+++ Player_files/Risky_dealer.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Dealer.h"
+
+#ifndef RISKY_DEALER_H
+#define RISKY_DEALER_H
+
+class Risky_Dealer: public Dealer {
+    private:
+        
+    public:
+
+}
+
+#endif
